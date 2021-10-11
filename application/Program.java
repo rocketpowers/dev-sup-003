84bc7b268812bf4f488491d86c46ea77511355c2
rocketpowers <rocketpowerswillian@gmail.com>
2021-10-11 11:44:23 -0300

    starting work 003

---
+++ application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		List <Employee> list = new ArrayList<>();
+		
+		
+		System.out.println("how many employees will be registered ? ");
+		int N = scan.nextInt();
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
